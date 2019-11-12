5a069af434fd333494581bf17b5629688c2210ac
trilamsr <trilam153@gmail.com>
2019-11-11 22:02:34 -0800

    Reorder Things

--- 0547_FriendCircles/FriendCircles.py
+++ 0547_FriendCircles/FriendCircles.py
@@ -1,3 +1,22 @@
+def dfs (M, row):
+    is_second_visit = M[row][row] == 0
+    if is_second_visit: return 0
+    M[row][row] = 0;
+    for col in range(len(M)):
+        is_friend = M[row][col]
+        if is_friend:
+            # M[row][col] = 0
+            # M[col][row] = 0 
+            dfs(M, col)
+    return 1
+
+class Solution:
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        if not M: return 0;
+        return sum(dfs(M, row) for row in range(len(M)))
+
+
+
 VISITED = 0
 
 def adjacent(mat, curr):
@@ -25,20 +44,3 @@ class Solution:
 
 
 
-
-def dfs (M, row):
-    is_second_visit = M[row][row] == 0
-    if is_second_visit: return 0
-    M[row][row] = 0;
-    for col in range(len(M)):
-        is_friend = M[row][col]
-        if is_friend:
-            # M[row][col] = 0
-            # M[col][row] = 0 
-            dfs(M, col)
-    return 1
-
-class Solution:
-    def findCircleNum(self, M: List[List[int]]) -> int:
-        if not M: return 0;
-        return sum(dfs(M, row) for row in range(len(M)))
